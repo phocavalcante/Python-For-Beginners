71aaf35004a12ed1b179270e10ac53b9a9fca67a
phocavalcante <pho.cavalcante@gmail.com>
2021-02-07 12:50:42 -0500

    Primeira aula

---
+++ Lesson_001/[Text].py
@@ -0,0 +1,18 @@
+'''
+Meu primeiro contato com Python foi através de uma vídeo aula no Youtube. Não conhecia absolutamente nada de programação, mas aquela aula me fez ter o desejo de aprender algo que parecia desafiador e ao mesmo tempo importante para a minha área de formação, tendo em vista as suas possibilidades de aplicação na Biologia.
+
+Não se preocupe que aqui não vamos tratar de conceitos e termos biológicos, apenas sobre Python e certamente vocês encontraram aplicações dessa linguaguem naquilo que gostam de fazer, e nas mais diversas área do conhecimento.
+
+Chega de delngas e vamos aprender o que julgo ser o básico dessa linguagem, conforme tópicos abaixo:
+
+
+
+
+
+
+
+
+
+
+
+'''
